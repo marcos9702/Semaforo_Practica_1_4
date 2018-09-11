4c2bfdaad61e74c4a15ab41706c7c11bff82564e
Marcos Flores <marcos9702@gmail.com>
2018-09-11 09:57:09 -0500

    *Inicial*

---
+++ Semaforo_Practica_1_4.ino
@@ -0,0 +1,74 @@
+#define led_verde 13
+#define led_amarillo 12
+#define led_rojo 11
+#define led_verdePeaton 10
+#define led_rojoPeaton 9
+#define pushBoton 7
+#define pot A5
+int valorPot=0;
+
+void setup(){
+  pinMode(led_verde,OUTPUT);
+  pinMode(led_amarillo,OUTPUT);
+  pinMode(led_rojo,OUTPUT);
+  pinMode(led_verdePeaton, OUTPUT);
+  pinMode(led_rojoPeaton, OUTPUT);
+  pinMode(pushBoton,INPUT_PULLUP);
+  Serial.begin(9600);
+  Peaton();
+  
+}
+void verde(int tiempo){
+  digitalWrite(led_verde,HIGH);
+  delay(tiempo);
+}
+
+void parpadeo(){
+  digitalWrite(led_verde,LOW);
+  delay(500);
+  for(int i=0;i<5;i++){ 
+    digitalWrite(led_verde,HIGH);
+    delay(500);
+    digitalWrite(led_verde,LOW);
+    delay(400);
+  }
+}
+
+void amarillo(){
+  digitalWrite(led_amarillo,HIGH);
+  delay(2000);
+  digitalWrite(led_amarillo,LOW);
+  delay(100);
+}
+
+void rojo(){
+  digitalWrite(led_rojo,HIGH);
+  Peaton();
+  delay(5000);
+  digitalWrite(led_rojo,LOW);
+  Peaton();
+  delay(100);
+}
+
+void Peaton(){
+  if(digitalRead(led_rojo) == LOW){
+    digitalWrite(led_verdePeaton,LOW);
+    digitalWrite(led_rojoPeaton,HIGH);
+  }else{
+    digitalWrite(led_verdePeaton,HIGH);
+    digitalWrite(led_rojoPeaton,LOW);  
+  }   
+}
+
+void loop(){  
+  digitalWrite(led_verde,HIGH);
+  Serial.print("Valor del potenciometro ");
+  Serial.println(valorPot);
+  valorPot=analogRead(pot);
+  if(digitalRead(pushBoton) == LOW){
+    verde(valorPot*10);  
+    parpadeo();
+    amarillo();
+    rojo();
+  }
+}
